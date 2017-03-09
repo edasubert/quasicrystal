9bed56ac676aa3a307bc0624d42de7177127cf57
edasubert <edasubert@gmail.com>
2017-03-09 16:34:14 +0100

    centralization of config

---
+++ config.h
@@ -0,0 +1,45 @@
+#if !defined(MYLIB_CONSTANTS_H)
+#define MYLIB_CONSTANTS_H 1
+
+#include "SUPPORT/betaSet.h"
+#include "SUPPORT/alphaSet.h"
+#include "SUPPORT/delone10.h"
+#include "SUPPORT/window2.h"
+#include "SUPPORT/generate3.h"
+#include "SUPPORT/geometricObject2.h"
+
+typedef betaSet numberType;
+//typedef rhombus<numberType> windowType;
+typedef polygon<numberType> windowType;
+
+windowType getWindow(numberType winSize)
+{
+  return polygon<numberType>::rhombic(winSize);
+  //return polygon<numberType>::octagon(winSize);
+  //return windowType(winSize);
+}
+
+numberType const const_winSize = numberType::get(1,0,1);
+
+// cell colors
+std::string const const_fillColor = "#689F38";
+std::string const const_strokeColor = "#263238";
+std::string const_strokeWidth(numberType winSize)
+{
+  std::ostringstream convert;
+  convert << (0.1)*winSize;
+  return convert.str();
+}
+  
+// window colors
+std::string const const_windowfillColor = "#F44336";
+std::string const const_windowstrokeColor = "#37474F";
+std::string const_windowstrokeWidth(numberType winSize)
+{
+  std::ostringstream convert;
+  convert << (0.003)*winSize;
+  return convert.str();
+}
+
+
+#endif
